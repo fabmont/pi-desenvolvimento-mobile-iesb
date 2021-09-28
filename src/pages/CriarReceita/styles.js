ba93a259ad1e257f6552595d11aaa5647c3ffcdd
fabmont <fabriciosantos47@gmail.com>
2021-09-28 12:02:05 -0300

    cria rotas e stacks de area privada e publica

--- src/pages/CriarReceita/styles.js
+++ src/pages/CriarReceita/styles.js
@@ -1,5 +1,9 @@
 import { StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
-  container: {},
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
 });
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,84 @@
 import React from 'react';
-import { View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import Login from './Login';
+import CriarConta from './CriarConta';
+import Feed from './Feed';
+import Favoritos from './Favoritos';
+import DetalhesReceita from './DetalhesReceita';
+import ModoPreparo from './ModoPreparo';
+import CriarReceita from './CriarReceita';
+import Perfil from './Perfil';
+import EditarPerfil from './EditarPerfil';
+
+const Stack = createNativeStackNavigator();
+const defaultRouteProps = {
+  options: {
+    headerShown: false,
+  },
+};
 
 export default function Pages() {
-  return <View />;
+  const isSigned = true;
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        {isSigned ? (
+          <>
+            <Stack.Screen name="Feed" component={Feed} {...defaultRouteProps} />
+            <Stack.Screen
+              name="Favoritos"
+              component={Favoritos}
+              {...defaultRouteProps}
+            />
+            <Stack.Screen
+              name="Detalhes"
+              component={DetalhesReceita}
+              {...defaultRouteProps}
+            />
+            <Stack.Screen
+              name="ModoPreparo"
+              component={ModoPreparo}
+              {...defaultRouteProps}
+            />
+            <Stack.Screen
+              name="EditarReceita"
+              component={EditarPerfil}
+              {...defaultRouteProps}
+            />
+            <Stack.Screen
+              name="CriarReceita"
+              component={CriarReceita}
+              {...defaultRouteProps}
+            />
+            <Stack.Screen
+              name="Perfil"
+              component={Perfil}
+              {...defaultRouteProps}
+            />
+            <Stack.Screen
+              name="EditarPerfil"
+              component={EditarPerfil}
+              {...defaultRouteProps}
+            />
+          </>
+        ) : (
+          <>
+            <Stack.Screen
+              name="Login"
+              component={Login}
+              {...defaultRouteProps}
+            />
+            <Stack.Screen
+              name="CriarConta"
+              component={CriarConta}
+              {...defaultRouteProps}
+            />
+          </>
+        )}
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
