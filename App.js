9a8cdae5c6ea914146d10a99c96b43afa4bce299
fabmont <fabriciosantos47@gmail.com>
2021-10-05 13:03:20 -0300

    adiciona temas no app

--- App.js
+++ App.js
@@ -1,10 +1,19 @@
+/* eslint-disable react/style-prop-object */
 import React from 'react';
 import * as eva from '@eva-design/eva';
-import { ApplicationProvider } from '@ui-kitten/components';
+import { ApplicationProvider, IconRegistry } from '@ui-kitten/components';
+import { EvaIconsPack } from '@ui-kitten/eva-icons';
+import { StatusBar } from 'expo-status-bar';
+
 import Pages from './src/pages';
+import theme from './src/constants/theme.json';
 
 export default () => (
-  <ApplicationProvider {...eva} theme={eva.light}>
-    <Pages />
-  </ApplicationProvider>
+  <>
+    <StatusBar animated style="dark" />
+    <IconRegistry icons={EvaIconsPack} />
+    <ApplicationProvider {...eva} theme={{ ...eva.light, ...theme }}>
+      <Pages />
+    </ApplicationProvider>
+  </>
 );
