5e9efc7c90981edf502431d063823fd946bcacf0
fabmont <fabriciosantos47@gmail.com>
2021-10-05 00:59:25 -0300

    cria pagina de feed

--- src/pages/Feed/index.js
+++ src/pages/Feed/index.js
@@ -1,12 +1,87 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import {
+  Keyboard,
+  TextInput,
+  View,
+  TouchableWithoutFeedback,
+  TouchableOpacity,
+  FlatList,
+} from 'react-native';
+import { Layout, Text } from '@ui-kitten/components';
+import { Feather } from '@expo/vector-icons';
 
 import styles from './styles';
+import CardReceita from '../../components/CardReceita';
 
 export default function Feed() {
+  const data = [
+    {
+      uuid: '2321jh32hg3u12g3u12',
+      title: 'Salada',
+      imgUrl:
+        'https://www.dicasdemulher.com.br/wp-content/uploads/2017/10/salada-caesar-receitas.jpg',
+      owner: 'John Due',
+      timeToPrepare: 15,
+      difficulty: 'Fácil',
+    },
+  ];
+
   return (
-    <View style={styles.container}>
-      <Text>Feed</Text>
-    </View>
+    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+      <Layout style={styles.container}>
+        <View style={styles.header}>
+          <Text category="h4" style={styles.headerTitle}>
+            Feed
+          </Text>
+
+          <View style={styles.searchContainer}>
+            <Feather name="search" size={20} color="#c9c9c9" />
+            <TextInput
+              style={styles.searchInput}
+              placeholderTextColor="#c9c9c9"
+              placeholder="Pesquisar receitas"
+            />
+          </View>
+
+          <View style={styles.filterRow}>
+            <TouchableOpacity activeOpacity={0.7} style={styles.filterBadge}>
+              <Feather
+                name="chevron-down"
+                color="#424242"
+                style={{ marginRight: 4 }}
+              />
+              <Text category="label" style={styles.filterBadgeText}>
+                Ordenar
+              </Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity activeOpacity={0.7} style={styles.filterBadge}>
+              <Text category="label" style={styles.filterBadgeText}>
+                Filtros
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+
+        <FlatList
+          data={[...data]}
+          style={{ paddingTop: 16 }}
+          renderItem={({
+            item: { title, imgUrl, owner, timeToPrepare, difficulty },
+          }) => (
+            <CardReceita
+              a={console.log(title)}
+              title={title}
+              imgUrl={imgUrl}
+              owner={owner}
+              timeToPrepare={timeToPrepare}
+              difficulty={difficulty}
+              onPress={() => null}
+            />
+          )}
+          keyExtractor={({ uuid }) => uuid}
+        />
+      </Layout>
+    </TouchableWithoutFeedback>
   );
 }
--- src/pages/Feed/styles.js
+++ src/pages/Feed/styles.js
@@ -1,9 +1,71 @@
 import { StyleSheet } from 'react-native';
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
+import {
+  heightPercentageToDP as hp,
+  widthPercentageToDP as wp,
+} from 'react-native-responsive-screen';
 
 export default StyleSheet.create({
   container: {
     flex: 1,
+    paddingTop: getStatusBarHeight(),
+  },
+  header: {
+    paddingHorizontal: wp(4),
+    paddingBottom: hp(2),
+    paddingTop: wp(2),
+    borderBottomColor: '#E4E4E4',
+    borderBottomWidth: 1,
+  },
+  headerTitle: {
+    marginBottom: hp(1),
+    fontFamily: 'Inter_700Bold',
+  },
+  searchContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#F9F9F9',
+    borderRadius: wp(2),
+    paddingHorizontal: wp(5),
+    paddingVertical: hp(1.5),
+  },
+  searchInput: {
+    flex: 1,
+    marginLeft: wp(4),
+    fontFamily: 'Inter_400Regular',
     justifyContent: 'center',
+  },
+  recipeCard: {
+    elevation: 6,
+    backgroundColor: 'white',
+    marginHorizontal: wp(4),
+    paddingHorizontal: wp(4),
+    paddingVertical: hp(2),
+    borderRadius: wp(2),
+  },
+  cardImage: {
+    height: hp(20),
+    width: wp(25),
+    borderRadius: wp(2),
+  },
+  filterBadge: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 100,
+    borderWidth: 1,
+    borderColor: '#E4E4E4',
+    paddingHorizontal: wp(3),
+    paddingVertical: hp(0.5),
+    marginTop: hp(2),
+    marginRight: wp(2),
+  },
+  filterBadgeText: {
+    fontFamily: 'Inter_400Regular',
+    color: '#424242',
+  },
+  filterRow: {
+    flexDirection: 'row',
     alignItems: 'center',
   },
 });
