7c5075718e875251575e804d749404d61ef76c7b
João Victor Souza da Silva <galego2012@gmail.com>
2021-10-25 20:43:21 -0300

    Analisar tela de CriarConta

--- src/pages/CriarConta/styles.js
+++ src/pages/CriarConta/styles.js
@@ -1,9 +1,17 @@
+/* eslint-disable prettier/prettier */
 import { StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    flexDirection: 'column',
+    padding: 10,
+  },
+  field: {
+    height: 45,
+    borderBottomWidth: 0,
+    borderBottomColor: '#bbb',
+    padding: 5,
+    marginBottom: 15,
   },
 });
