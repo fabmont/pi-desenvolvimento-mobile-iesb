028b2d01d8fae1fc71101545dd133b4e2c62e5be
fabmont <fabriciosantos47@gmail.com>
2021-10-26 20:32:58 -0300

    cria tela de login

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,12 +1,32 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Image, TouchableWithoutFeedback, Keyboard } from 'react-native';
+import { Button, Input, Layout, Text } from '@ui-kitten/components';
 
 import styles from './styles';
+import loginCover from '../../assets/login-cover.png';
+import PasswordInput from '../../components/PasswordInput';
 
 export default function Login() {
   return (
-    <View style={styles.container}>
-      <Text>Login</Text>
-    </View>
+    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+      <Layout style={styles.container}>
+        <Image style={styles.cover} source={loginCover} resizeMode="stretch" />
+        <Layout style={styles.body}>
+          <View style={{ width: '100%' }}>
+            <Text category="h4" style={styles.title}>
+              Login
+            </Text>
+            <Input label="E-mail" style={styles.inputs} />
+            <PasswordInput label="Senha" style={styles.inputs} />
+            <Button style={styles.loginBtn}>Entrar</Button>
+          </View>
+
+          <View style={{ width: '100%' }}>
+            <Text style={styles.signupLabel}>Não tem uma conta ainda?</Text>
+            <Button appearance="ghost">Cadastre-se</Button>
+          </View>
+        </Layout>
+      </Layout>
+    </TouchableWithoutFeedback>
   );
 }
--- src/pages/Login/styles.js
+++ src/pages/Login/styles.js
@@ -1,9 +1,39 @@
 import { StyleSheet } from 'react-native';
+import {
+  heightPercentageToDP as hp,
+  widthPercentageToDP as wp,
+} from 'react-native-responsive-screen';
 
 export default StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+  },
+  cover: {
+    width: '100%',
+    height: hp(42),
+  },
+  body: {
+    flex: 1,
+    paddingHorizontal: wp(4),
+    justifyContent: 'space-evenly',
     alignItems: 'center',
+  },
+  title: {
+    fontFamily: 'Inter_700Bold',
+    marginBottom: hp(2),
+    width: '100%',
+  },
+  inputs: {
+    marginBottom: hp(2),
+  },
+  loginBtn: {
+    marginTop: hp(2),
+    marginBottom: hp(3),
+    width: '100%',
+  },
+  signupLabel: {
+    fontFamily: 'Inter_400Regular',
+    width: '100%',
+    textAlign: 'center',
   },
 });
