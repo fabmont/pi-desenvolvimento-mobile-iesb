7465f043082e2793ca3dad78e5d438d094c46213
fabmont <fabriciosantos47@gmail.com>
2021-10-25 21:18:54 -0300

    cria tela de criar receita

--- src/pages/CriarReceita/index.js
+++ src/pages/CriarReceita/index.js
@@ -1,12 +1,125 @@
+import {
+  Button,
+  Divider,
+  Icon,
+  Input,
+  Layout,
+  List,
+  ListItem,
+  Select,
+  SelectItem,
+  Text,
+} from '@ui-kitten/components';
 import React from 'react';
-import { View, Text } from 'react-native';
+import { ScrollView, View } from 'react-native';
+import Toolbar from '../../components/Toolbar';
 
 import styles from './styles';
+import categories from '../../constants/categories';
 
 export default function CriarReceita() {
+  const renderItemAccessory = () => (
+    <Button
+      size="small"
+      appearance="ghost"
+      accessoryLeft={(props) => <Icon name="close" {...props} />}
+    />
+  );
+
+  const renderItem = ({ item, index }) => (
+    <ListItem
+      title={`${item.title} ${index + 1}`}
+      accessoryRight={renderItemAccessory}
+    />
+  );
+
   return (
-    <View style={styles.container}>
-      <Text>CriarReceita</Text>
-    </View>
+    <Layout style={styles.container}>
+      <Toolbar hasBackButton title="Criar receita" />
+      <ScrollView contentContainerStyle={styles.body}>
+        <Input style={styles.input} label="Nome da receita" />
+        <Input
+          style={styles.input}
+          label="Descrição da receita"
+          multiline
+          textStyle={{ minHeight: 64 }}
+        />
+        <Select
+          label="Categoria da receita"
+          placeholder="Selecione uma categoria..."
+          style={styles.input}
+        >
+          {categories.map((cat) => (
+            <SelectItem key={cat.id} title={cat.label} />
+          ))}
+        </Select>
+
+        <View style={styles.row}>
+          <Input
+            style={{ ...styles.input, flex: 1 }}
+            label="Tempo de preparo"
+            placeholder="15"
+            keyboardType="numeric"
+            accessoryRight={(props) => <Text {...props}>min</Text>}
+          />
+          <View style={{ width: 16 }} />
+          <Input
+            style={{ ...styles.input, flex: 1 }}
+            label="Rendimento (pessoas)"
+            placeholder="0"
+            keyboardType="numeric"
+          />
+        </View>
+
+        <View style={styles.section}>
+          <Text style={styles.sectionText}>Ingredientes</Text>
+        </View>
+        <List
+          data={new Array(2).fill({
+            title: 'Title for Item',
+          })}
+          renderItem={renderItem}
+          ItemSeparatorComponent={Divider}
+          style={styles.input}
+        />
+        <View style={styles.centered}>
+          <Button
+            size="small"
+            appearance="outline"
+            accessoryLeft={(props) => <Icon name="plus" {...props} />}
+          >
+            Adicionar ingrediente
+          </Button>
+        </View>
+
+        <View style={styles.section}>
+          <Text style={styles.sectionText}>Modo de preparo</Text>
+        </View>
+        <List
+          data={new Array(2).fill({
+            title: 'Title for Item',
+          })}
+          renderItem={renderItem}
+          ItemSeparatorComponent={Divider}
+          style={styles.input}
+        />
+        <View style={styles.centered}>
+          <Button
+            size="small"
+            appearance="outline"
+            accessoryLeft={(props) => <Icon name="plus" {...props} />}
+          >
+            Adicionar novo passo
+          </Button>
+        </View>
+      </ScrollView>
+      <Button
+        size="large"
+        style={styles.submitBtn}
+        accessoryLeft={(props) => <Icon name="corner-up-right" {...props} />}
+      >
+        Enviar receita
+      </Button>
+    </Layout>
   );
 }
--- src/pages/CriarReceita/styles.js
+++ src/pages/CriarReceita/styles.js
@@ -1,9 +1,50 @@
 import { StyleSheet } from 'react-native';
+import {
+  getBottomSpace,
+  getStatusBarHeight,
+} from 'react-native-iphone-x-helper';
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from 'react-native-responsive-screen';
 
 export default StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    paddingTop: getStatusBarHeight(),
+    paddingBottom: getBottomSpace(),
+  },
+  body: {
+    paddingHorizontal: wp(4),
+    paddingVertical: hp(2),
+  },
+  input: {
+    marginBottom: hp(3),
+  },
+  row: {
+    width: '100%',
+    flexDirection: 'row',
     alignItems: 'center',
+  },
+  section: {
+    padding: wp(3),
+    backgroundColor: '#f5f5f5',
+    width: '100%',
+    alignItems: 'center',
+    borderRadius: wp(2),
+    marginBottom: hp(0.5),
+  },
+  sectionText: {
+    fontFamily: 'Inter_400Regular',
+    opacity: 0.6,
+  },
+  centered: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: hp(3),
+  },
+  submitBtn: {
+    borderRadius: 0,
+    marginTop: hp(2),
   },
 });
