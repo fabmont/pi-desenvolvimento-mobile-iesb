7b511b10e49985c7c6eb9096ed7f96416ccfada8
judsonmillerd <83422031+judsonmillerd@users.noreply.github.com>
2021-10-26 17:44:54 -0300

    Analisar tela Editar Perfil

--- src/pages/EditarPerfil/index.js
+++ src/pages/EditarPerfil/index.js
@@ -1,12 +1,70 @@
-import React from 'react';
-import { View, Text } from 'react-native';
-
+import React, { useState } from 'react';
+import {  View,TouchableOpacity,TextInput,Text} from 'react-native';
+import Toolbar from '../../components/Toolbar';
 import styles from './styles';
+import { Avatar } from 'react-native-elements';
 
-export default function EditarPerfil() {
+export default function EditarPerfil(){
   return (
-    <View style={styles.container}>
-      <Text>EditarPerfil</Text>
+  
+    <View style={styles.screen} >
+      <View style={styles.container}>
+        <Toolbar>
+        <ToolbarBackAction
+        onPress={this._goBack}
+    />
+      <ToolbarContent  title="Editar Perfil"/>
+      </Toolbar>
+      </View>
+<Avatar  
+size= {100}
+
+  onPress={() => console.log("Works!")}
+  activeOpacity={0.7}
+  rounded
+   source={{
+              uri: 'https://randomuser.me/api/portraits/men/79.jpg',}}
+  
+/><Text style={{ marginTop:10, textAlign: 'center' }}>Alterar foto</Text>
+
+<View style={styles.nomebox}>
+      <TextInput
+        placeholder="Nome"
+        style={styles.TextInput}
+        onChangeText={(Text) }
+      />
+  </View>
+<View style={styles.emailbox}>
+      <TextInput
+        placeholder="E-mail"
+        style={styles.TextInput}
+         keyboardType={'email-adress'}
+       
+      />
+  </View>
+  <View style={styles.idadebox}>
+      <TextInput
+        placeholder="Idade"
+        style={styles.TextInput}
+      />
+  </View>
+  <View style={styles.profissaobox}>
+      <TextInput
+        placeholder="Profissão"
+        style={styles.TextInput}
+        onChangeText={(Text)}
+      />
+  </View>
+<View style={styles.biobox}>
+      <TextInput
+        placeholder="Bio"
+        style={styles.TextInput}
+        onChangeText={(Text)}
+      />
+  </View>
+  <TouchableOpacity style={styles.alteracaobox} >
+        <Text style={{color: '#FFFFFF', textAlign: 'center' }}>SALVAR ALTERAÇÕES</Text>
+      </TouchableOpacity>
     </View>
   );
 }
--- src/pages/EditarPerfil/styles.js
+++ src/pages/EditarPerfil/styles.js
@@ -1,9 +1,96 @@
 import { StyleSheet } from 'react-native';
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
+import {
+  heightPercentageToDP as hp,
+  widthPercentageToDP as wp,
+} from 'react-native-responsive-screen';
 
 export default StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    paddingTop: getStatusBarHeight(),
   },
-});
+    screen: {
+     flex: 1, 
+     marginTop:60,
+      alignItems: 'center',
+      
+    },
+    nomebox:{
+       width: '93%',
+     marginTop:27,
+      height: 38,
+      top:1,
+      fontSize:20,
+      marginLeft:30,
+      marginRight:30,
+      backgroundColor: '#eaeaea',
+      borderRadius: 10,
+      paddingLeft: 10,
+      marginBottom:-4,
+      padding: 10,
+      
+    },
+  emailbox:{
+     width: '93%',
+     marginTop:27,
+      height: 38,
+      fontSize:20,
+      marginLeft:30,
+      marginRight:30,
+      backgroundColor: '#eaeaea',
+      borderRadius: 10,
+      paddingLeft: 10,
+      marginBottom: -4,
+     padding: 10,
+  },
+  idadebox:{
+   width: '93%',
+     marginTop:27,
+      height: 38,
+      fontSize:20,
+      marginLeft:30,
+      marginRight:30,
+      backgroundColor: '#eaeaea',
+      borderRadius: 10,
+      paddingLeft: 10,
+      marginBottom:-4,
+      padding: 10,
+  },
+  profissaobox:{
+    width: '93%',
+     marginTop:27,
+      height: 38,
+      fontSize:20,
+      marginLeft:30,
+      marginRight:30,
+      backgroundColor: '#eaeaea',
+      borderRadius: 10,
+      paddingLeft: 10,
+      marginBottom:-4,
+      padding: 10,
+  },
+  biobox:{
+    width: '93%',
+     marginTop:27,
+      height: 38,
+      fontSize:20,
+      marginLeft:30,
+      marginRight:30,
+      backgroundColor: '#eaeaea',
+      borderRadius: 10,
+      paddingLeft: 10,
+      marginBottom:20,
+      padding: 10,
+     
+  
+  },
+  alteracaobox:{
+    width: '93%',
+      height: 30,
+      backgroundColor: '#f5a623',
+      borderRadius: 5,
+      justifyContent: 'center',
+  }
+  
+  });
